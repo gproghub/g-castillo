27237fc7f55a73eb480ceb4c82c67843f19a655a
gdevcodes <gprog@tutanota.com>
2023-08-10 15:29:02 +0200

    autenticacion terminada

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-between px-3 py-3">
+      {/* <div className="flex gap-x-7 items-center justify-center"> */}
+      {/* <h1 className="text-xl md:text-3xl">Castillo</h1> */}
+      {/* {userId && <MainNav />} */}
+      {/* </div> */}
+
+      <div className="flex items-center gap-x-3">
+        {/* <ModeToggle /> */}
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
