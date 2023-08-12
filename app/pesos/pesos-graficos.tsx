d48a0d28f8f0e01d5bacd75e73a60a38b1db9d95
gdevcodes <gprog@tutanota.com>
2023-08-12 11:34:13 +0200

    estilos graficos terminados

--- app/pesos/pesos-graficos.tsx
+++ app/pesos/pesos-graficos.tsx
@@ -23,12 +23,17 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
   }));
   console.log(formattedPesos);
 
+  const tooltipStyle = {
+    backgroundColor: '#10b981',
+    borderRadius: '0.75rem',
+  };
+
   return (
     <div className="flex flex-col  gap-10">
       {/*  */}
       {/* MASA */}
 
-      <div className="h-96 w-5/6 mx-auto my-3">
+      <div className="h-96 w-full mx-auto my-3">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Masa (kg)</h1>
         <ResponsiveContainer>
           <LineChart
@@ -37,14 +42,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 30,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
-            <YAxis dataKey="masa" tickSize={10} domain={[74, 82]} />
-            <Tooltip />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
+            <YAxis dataKey="masa" tickSize={10} domain={[74, 83]} />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="masa"
@@ -58,7 +63,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/* GRASA CORPORAL */}
 
-      <div className="h-96 w-5/6 mx-auto my-3 ">
+      <div className="h-96 w-full mx-auto my-3 ">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Grasa Corporal (%)</h1>
         <ResponsiveContainer>
           <LineChart
@@ -67,14 +72,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="grasaCorporal" tickSize={10} domain={[5, 15]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="grasaCorporal"
@@ -88,7 +93,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/* AGUA */}
 
-      <div className="h-96 w-5/6 mx-auto my-3">
+      <div className="h-96 w-full mx-auto my-3">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Agua (%)</h1>
         <ResponsiveContainer>
           <LineChart
@@ -97,14 +102,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="agua" tickSize={10} domain={[58, 70]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="agua"
@@ -118,7 +123,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/* GRASA VISCERAL */}
 
-      <div className="h-96 w-5/6 mx-auto my-3 ">
+      <div className="h-96 w-full mx-auto my-3 ">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Grasa Visceral </h1>
         <ResponsiveContainer>
           <LineChart
@@ -127,14 +132,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="grasaVisceral" tickSize={10} domain={[0, 5]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="grasaVisceral"
@@ -148,7 +153,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/* MÚSCULO */}
 
-      <div className="h-96 w-5/6 mx-auto my-3">
+      <div className="h-96 w-full mx-auto my-3">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Músculo (kg)</h1>
         <ResponsiveContainer>
           <LineChart
@@ -157,14 +162,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="musculo" tickSize={10} domain={[62, 76]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="musculo"
@@ -178,7 +183,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/* NIVEL FÍSICO */}
 
-      <div className="h-96 w-5/6 mx-auto my-3">
+      <div className="h-96 w-full mx-auto my-3">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Nivel Físico</h1>
         <ResponsiveContainer>
           <LineChart
@@ -187,14 +192,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="nivelFisico" tickSize={10} domain={[2, 10]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="nivelFisico"
@@ -208,7 +213,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/* HUESO */}
 
-      <div className="h-96 w-5/6 mx-auto my-3">
+      <div className="h-96 w-full mx-auto my-3">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Hueso (kg)</h1>
         <ResponsiveContainer>
           <LineChart
@@ -217,14 +222,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="hueso" tickSize={10} domain={[2, 5]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="hueso"
@@ -238,7 +243,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/* ENERGÍA */}
 
-      <div className="h-96 w-5/6 mx-auto my-3">
+      <div className="h-96 w-full mx-auto my-3">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Energía (kCal)</h1>
         <ResponsiveContainer>
           <LineChart
@@ -247,14 +252,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="energia" tickSize={10} domain={[1900, 2200]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="energia"
@@ -268,7 +273,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/*EDAD METABÓLICA */}
 
-      <div className="h-96 w-5/6 mx-auto my-3">
+      <div className="h-96 w-full mx-auto my-3">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">Edad Metabólica</h1>
         <ResponsiveContainer>
           <LineChart
@@ -277,14 +282,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="edadMetabolica" tickSize={10} domain={[15, 30]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="edadMetabolica"
@@ -298,7 +303,7 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
       {/*  */}
       {/* IMC */}
 
-      <div className="h-96 w-5/6 mx-auto my-3">
+      <div className="h-96 w-full mx-auto my-3">
         <h1 className="text-base md:text-xl lg:text-2xl mb-2">IMC</h1>
         <ResponsiveContainer>
           <LineChart
@@ -307,14 +312,14 @@ const PesosGraficos: React.FC<PesosGraficosProps> = ({ pesos }) => {
               top: 30,
               right: 30,
               left: 0,
-              bottom: 15,
+              bottom: 35,
             }}
             className="text-sm shadow-md shadow-emerald-500 rounded-xl bg-white dark:bg-black"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="fecha" tickSize={10} reversed />
+            <XAxis dataKey="fecha" tickSize={20} reversed interval={0} angle={315} />
             <YAxis dataKey="imc" tickSize={10} domain={[20, 25]} />
-            <Tooltip />
+            <Tooltip contentStyle={tooltipStyle} itemStyle={{ color: 'black' }} />
             <Line
               type="monotone"
               dataKey="imc"
